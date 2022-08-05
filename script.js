469ddd60a51e96e8f3b13360b25c8d04d070f724
Mubarak2278 <bw80452@gmail.com>
2022-08-05 07:40:24 -0700

    This is a new repo

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = ducument.getElementsByClassName('toggle-button')
+const nav = document.getElementsByClassName('ul')[0]
+
+toggleButton.addEventListener('click', () => {
+    nav, li.ClassList.toggle('active')
+})
